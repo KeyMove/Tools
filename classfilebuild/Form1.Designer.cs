096f1119be0dbb74986ccefd408f94b8beb000d8
KeyMove <keymove@live.com>
2017-01-04 17:08:26 +0800

    c语言头文件生产

---
+++ classfilebuild/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace classfilebuild
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.mcuhead = new System.Windows.Forms.CheckBox();
+            this.ListImage = new System.Windows.Forms.ImageList(this.components);
+            this.FileNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Location = new System.Drawing.Point(108, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.DragDrop += new System.Windows.Forms.DragEventHandler(this.button1_DragDrop);
+            this.button1.DragEnter += new System.Windows.Forms.DragEventHandler(this.button1_DragEnter);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.exit);
+            // 
+            // mcuhead
+            // 
+            this.mcuhead.AutoSize = true;
+            this.mcuhead.Checked = true;
+            this.mcuhead.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mcuhead.Location = new System.Drawing.Point(12, 111);
+            this.mcuhead.Name = "mcuhead";
+            this.mcuhead.Size = new System.Drawing.Size(90, 16);
+            this.mcuhead.TabIndex = 1;
+            this.mcuhead.Text = "生成mcuhead";
+            this.mcuhead.UseVisualStyleBackColor = true;
+            // 
+            // ListImage
+            // 
+            this.ListImage.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ListImage.ImageStream")));
+            this.ListImage.TransparentColor = System.Drawing.Color.Transparent;
+            this.ListImage.Images.SetKeyName(0, "QQ截图20160418160518.jpg");
+            // 
+            // FileNameBox
+            // 
+            this.FileNameBox.Location = new System.Drawing.Point(75, 6);
+            this.FileNameBox.Name = "FileNameBox";
+            this.FileNameBox.Size = new System.Drawing.Size(100, 21);
+            this.FileNameBox.TabIndex = 3;
+            this.FileNameBox.TextChanged += new System.EventHandler(this.FileNameBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "文件名称:";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("宋体", 24F);
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.ListImage;
+            this.treeView1.Location = new System.Drawing.Point(12, 33);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.ShowLines = false;
+            this.treeView1.ShowPlusMinus = false;
+            this.treeView1.ShowRootLines = false;
+            this.treeView1.Size = new System.Drawing.Size(163, 67);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listView1_ItemDrag);
+            this.treeView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.button1_DragDrop);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 135);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileNameBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mcuhead);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "C语言文件生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.exit);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox mcuhead;
+        private System.Windows.Forms.ImageList ListImage;
+        private System.Windows.Forms.TextBox FileNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ classfilebuild/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace classfilebuild
+{
+    public partial class Form1 : Form
+    {
+
+        const string mcuheaddata = "#ifndef _mcuhead_H_\r\n#define _mcuhead_H_\r\n\r\n#define u8 unsigned char\r\n#define u16 unsigned short\r\n#define u32 unsigned long\r\n\r\n#define s8 signed char\r\n#define s16 signed short\r\n#define s32 signed long\r\n\r\n#define vu8 volatile u8\r\n#define vu16 volatile u16\r\n#define vu32 volatile u32\r\n\r\n\r\n#define BIT0 1\r\n#define BIT1 2\r\n#define BIT2 4\r\n#define BIT3 8\r\n#define BIT4 0x10\r\n#define BIT5 0x20\r\n#define BIT6 0x40\r\n#define BIT7 0x80\r\n#define BIT8 0x100\r\n#define BIT9 0x200\r\n#define BIT10 0x400\r\n#define BIT11 0x800\r\n#define BIT12 0x1000\r\n#define BIT13 0x2000\r\n#define BIT14 0x4000\r\n#define BIT15 0x8000\r\n#define BIT16 0x10000\r\n#define BIT17 0x20000\r\n#define BIT18 0x40000\r\n#define BIT19 0x80000\r\n#define BIT20 0x100000\r\n#define BIT21 0x200000\r\n#define BIT22 0x400000\r\n#define BIT23 0x800000\r\n#define BIT24 0x1000000\r\n#define BIT25 0x2000000\r\n#define BIT26 0x4000000\r\n#define BIT27 0x8000000\r\n#define BIT28 0x10000000\r\n#define BIT29 0x20000000\r\n#define BIT30 0x40000000\r\n#define BIT31 0x80000000\r\n\r\n\r\n#define SETBIT(x,y) (x)|=(y)\r\n#define CLRBIT(x,y) (x)&=~(y)\r\n#define CPLBIT(x,y) (x)^=(y);\r\n\r\nunion u2i{\r\n	vu32 l;\r\n	vu16 li[2];\r\n	vu8 lc[4];\r\n	vu16 i;\r\n	vu8 c[2];\r\n};\r\n\r\n#define _16T8H(x) (((union u2i*)&x)->c[1])//??16??8?\r\n#define _16T8L(x) (((union u2i*)&x)->c[0])//??16??8?\r\n\r\n#define _32T16H(x) (((union u2i*)&x)->li[1])//??32??16?\r\n#define _32T16L(x) (((union u2i*)&x)->li[0])//??32??16?\r\n\r\n#define _32T8HH(x) (((union u2i*)&x)->lc[3])//??32??8?\r\n#define _32T8H(x) (((union u2i*)&x)->lc[2])//??32???8?\r\n#define _32T8L(x) (((union u2i*)&x)->lc[1])//??32???8?\r\n#define _32T8LL(x) (((union u2i*)&x)->lc[0])//??32?8?\r\n\r\n#define BIT(x) (((x>>21)&BIT9)|((x>>24)&BIT8)|((x>>21)&BIT7)|((x>>18)&BIT6)|((x>>15)&BIT5)|((x>>12)&BIT4)|((x>>9)&BIT3)|((x>>6)&BIT2)|((x>>3)&BIT1)|((x)&BIT0))\r\n#define BIN(X) BIT(0x##X)\r\n#define BINBYTE(b) BIM(b)\r\n#define BINWORD(b1,b2) ((BIN(b1)<<8)|BIN(b2))\r\n#define BINDWORD(b1,b2,b3,b4) ((BIN(b1)<<24)|(BIN(b2)<<16)|(BIN(b3)<<8)|BIN(b4))\r\n\r\n#endif\r\n";
+        const string headdata = "#ifndef _{0:s}_H_\r\n#define _{0:s}_H_\r\n\r\n{1:s}\r\n{2:s}\r\ntypedef struct {{\r\n\tvoid(*Init)(void);\r\n}}{0:s}Base;\r\n\r\nextern const {0:s}Base {0:s};\r\n\r\n#endif\r\n";
+        const string sourdata = "#include \"{0:s}.h\"\r\nstatic void init(){{\r\n}}\r\n\r\nconst {0:s}Base {0:s} = {{\r\n\tinit,\t\r\n}};\r\n";
+        const string nomcuhead = "#define u8 unsigned char\r\n#define u16 unsigned short\r\n#define u32 unsigned long\r\n\r\n";
+        const string ismcuhead = "#include \"mcuhead.h\"";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void buildfile(string path,string o)
+        {
+            System.IO.FileStream f = new System.IO.FileStream(path, System.IO.FileMode.Create);
+            char[] cl = o.ToCharArray();
+            byte[] bl = new byte[cl.Length * 2];
+            int v = 0;
+            int blen = 0;
+            for (int i = 0; i < cl.Length; i++)
+            {
+                v = cl[i];
+                if (v < 128)
+                {
+                    bl[blen++] = (byte)v;
+                }
+                else
+                {
+                    bl[blen++] = (byte)(v >> 8);
+                    bl[blen++] = (byte)v;
+                }
+            }
+            f.Write(bl, 0, blen);
+            f.Flush();
+            f.Close();
+        }
+
+        void buildfile(string path)
+        {
+            string add="";
+            if (path.IndexOf('.') != -1)
+            {
+                path = path.Substring(0, path.IndexOf('.'));
+            }
+
+            string name = path.Substring(path.LastIndexOf('\\') + 1);
+            path = path.Substring(0, path.LastIndexOf('\\') + 1);
+            string headchose = nomcuhead;
+            if (mcuhead.Checked)
+            {
+                if (!System.IO.File.Exists(path + "mcuhead.h"))
+                {
+                    buildfile(path + "mcuhead.h", mcuheaddata);
+                }
+                headchose = ismcuhead;
+            }
+            buildfile(path + name + ".h", string.Format(headdata, name, headchose, add));
+            buildfile(path + name + ".c", string.Format(sourdata, name));
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "(源文件/头文件 .c/.h)|*.*";
+            string add = "";
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+                buildfile(s.FileName);
+                return;
+                string path = s.FileName;
+                if (path.IndexOf('.') != -1)
+                {
+                    path = path.Substring(0, path.IndexOf('.'));
+                }
+                
+                string name = path.Substring(path.LastIndexOf('\\')+1);
+                path = path.Substring(0, path.LastIndexOf('\\')+1);
+                string headchose = nomcuhead;
+                if (mcuhead.Checked)
+                {
+                    if (!System.IO.File.Exists(path + "mcuhead.h"))
+                    {
+                        buildfile(path + "mcuhead.h",mcuheaddata);
+                    }
+                    headchose = ismcuhead;
+                }   
+                buildfile(path +name+ ".h", string.Format(headdata, name, name, headchose, add));
+                buildfile(path +name+ ".c", string.Format(sourdata, name));
+            }
+        }
+
+        private void exit(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar==(char)Keys.Escape)
+                System.Environment.Exit(0);
+        }
+
+        private void button1_DragEnter(object sender, DragEventArgs e)
+        {
+            //MessageBox.Show("拖放完成1");
+            //e.Effect = DragDropEffects.Copy | DragDropEffects.Move;
+        }
+
+        private void button1_DragDrop(object sender, DragEventArgs e)
+        {
+            //MessageBox.Show("拖放完成2");
+            //e.Effect = DragDropEffects.Copy | DragDropEffects.Move;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //listView1.Items.Add(v);
+        }
+
+        private void listView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            if (string.Empty == FileNameBox.Text) return;
+            string path = System.Environment.CurrentDirectory+"\\tmp\\";
+            if (!File.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (!File.Exists(path + FileNameBox.Text + ".c"))
+            {
+               buildfile(path+ FileNameBox.Text);
+            }
+            treeView1.DoDragDrop(new DataObject(DataFormats.FileDrop,new string[]{ path + FileNameBox.Text+".c", path + FileNameBox.Text + ".h" }), DragDropEffects.Copy);
+            Directory.Delete(path,true);
+            //File.Delete(path);
+            //File.Delete(path + FileNameBox.Text + ".h");
+        }
+
+        private void FileNameBox_TextChanged(object sender, EventArgs e)
+        {
+            if (((Control)sender).Text == string.Empty)
+            {
+                if (treeView1.Nodes.Count != 0)
+                {
+                    treeView1.Nodes.Clear();
+                }
+                return;
+            }
+            if (treeView1.Nodes.Count == 0)
+            {
+                treeView1.Nodes.Add(new TreeNode());
+                treeView1.Nodes[0].ImageIndex = 0;
+            }
+            treeView1.Nodes[0].Text = ((Control)sender).Text;
+        }
+    }
+}
